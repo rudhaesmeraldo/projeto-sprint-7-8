a06d0d8387d0b5b498bde7df1f07e25d49ed5aab
rudha <rudhaesmeraldo@gmail.com>
2025-09-19 16:50:23 -0300

    ✨ feat(app): integra a lógica do chatbot RAG no servidor Flask

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import logging
+from src.chatbot import busca_por_similaridade, gera_resposta
 
 logging.basicConfig(level=logging.INFO)
 
@@ -10,13 +11,35 @@ app = Flask(__name__)
 def telegram_webhook():
     # Pega os dados json enviado
     update = request.get_json()
-
-    # logging para ver o que recebemos
     app.logger.info('Recebido um update: %s', update)
 
-    # ainda será necessário chamar a lógica do chatbot
+    if 'message' in update and 'text' in update['message']:
+        chat_id = update['message']['chat']['id']
+        texto_do_usuario = update['message']['text']
+        app.logger.info(f'Chat ID: {chat_id}, Mensagem: "{texto_do_usuario}"')
+
+        # pega o documento similar do ChromaDB
+        resultados_similares = busca_por_similaridade(texto_do_usuario)
+
+        # gera a resposta usando o Bedrock
+        resposta_bedrock = gera_resposta(resultados_similares, texto_do_usuario)
+        
+        # extrai o conteúdo da resposta
+        reposta_do_bot = resposta_bedrock.content
+        app.logger.info(f'Resposta gerada: "{reposta_do_bot}"')
+
+        # a resposta para o webhook do telegram é um json instruindo o que fazer, então preciso dizer para o telegram enviar a minha resposta de volta para o chat de onde a mensagem veio
+        response_data = {
+            'method': 'sendMessage',
+            'chat_id': chat_id,
+            'text': reposta_do_bot
+        }
+        
+        return jsonify(response_data)
     
+    # vou garantir pelo menos que retorne 200 OK
     return 'OK', 200
 
+# vou manter apenas para meus testes, mas vou remover posteriormente
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
