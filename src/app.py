8c574df5c795a0e6b8a6882711557e1c150a12cb
rudha <rudhaesmeraldo@gmail.com>
2025-09-25 01:35:23 -0300

    ♻️ refactor: altera lógica de resposta para uma chamada explícita à API do Telegram

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 import logging
 from src.chatbot import gera_resposta
+import os, requests
 
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
+TOKEN = os.getenv('TELEGRAM_BOT_API_KEY')
+TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
 # Define a endpoint tipo POST que vai receber as mensagens
 @app.route('/webhook', methods=['POST'])
@@ -22,14 +25,26 @@ def telegram_webhook():
         reposta_do_bot = gera_resposta(texto_do_usuario, chat_id)
         app.logger.info(f'Resposta gerada: "{reposta_do_bot}"')
 
-        # a resposta para o webhook do telegram é um json instruindo o que fazer, então preciso dizer para o telegram enviar a minha resposta de volta para o chat de onde a mensagem veio
-        response_data = {
-            'method': 'sendMessage',
+        # monta o payload para a API do Telegram
+        payload = {
             'chat_id': chat_id,
             'text': reposta_do_bot
         }
+
+        # envia a mensagem fazendo uma nova requisição
+        requests.post(TELEGRAM_API_URL, json=payload)
+
+        # envia a resposta para o Telegram
+        return 'OK', 200
+    
+        # # a resposta para o webhook do telegram é um json instruindo o que fazer, então preciso dizer para o telegram enviar a minha resposta de volta para o chat de onde a mensagem veio
+        # response_data = {
+        #     'method': 'sendMessage',
+        #     'chat_id': chat_id,
+        #     'text': reposta_do_bot
+        # }
         
-        return jsonify(response_data)
+        # return jsonify(response_data)
     
     # vou garantir pelo menos que retorne 200 OK
     return 'OK', 200
